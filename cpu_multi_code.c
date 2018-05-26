375b8dfe6eab1aedac76cd6b43991b07c68fcb72
Paulo Bodnarchuki <paulobodna@hotmail.com>
2018-05-26 19:45:41 -0300

    Modularizacao, funcao de escrita na RAM, structs de componentes da cpu

--- cpu_multi_code.c
+++ cpu_multi_code.c
@@ -1,42 +1,132 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+typedef struct ula {
+    int zero;
+    int aluout;
+} ULA;
+
+typedef struct ir {
+    int *inst;
+} IR;
+
+typedef struct regs {
+    int *registers;
+} REGS;
 
 const int R = 256;	// tamanho da RAM em bytes
+
+int init_cpu(int ***RAM, IR **ir, REGS **regs) {
+
+	*RAM = (int **) calloc (R, sizeof(int *));
+	for (int i = 0; i < R; i++) (*RAM)[i] = (int *) calloc (8, sizeof(int));
+
+    *ir = (IR *) calloc(32, sizeof(IR));
+    *regs = (REGS *) calloc(32, sizeof(REGS)); 
+
+    return 1;
+}
+
+int free_cpu(int ***RAM, IR **ir, REGS **regs) {
+
+	for (int i = 0; i < R; i++) free((*RAM)[i]);
+	free(*RAM);
+    *RAM = NULL;
+    
+    free(*ir);
+    *ir = NULL;
+    free(*regs);
+    *regs = NULL;
+
+    return 1;
+}
+
+int dec_to_bin(unsigned int n, int *bin){
+
+	int size = 32;
+
+	while (size > 0) {
+		bin[size-1] = n % 2;
+		n /= 2;
+		size--;
+	}
+
+    return 1;
+}
+
+// a chamada dessa função pode ocorrer quando a word de 32 bits for colocada no IR
+int bin_to_dec(int* bin_num, int start_pos, int end_pos) { 
+	int dec = 0;
+	int exp = 0;
+
+	for (int i = end_pos; i >= start_pos; i--) dec += bin_num[i] * pow(2, exp++);
+
+    return dec;
+}
+
+int RAM_write(int **RAM, int start_pos, int *bin) {
+   
+   int i, j; 
+    for(i = 0, j = start_pos; i < 32; i+= 8, j++) {
+        memcpy(RAM[j], &(bin[i]), 8*sizeof(int));
+    }
+
+    return 1;
+}
+
+int readfile(char *file_name, int **RAM) {
+	unsigned int inst;
+
+	FILE *fp = NULL;
+	fp = fopen(file_name, "r");
+
+    if(fp == NULL) {
+        printf("Arquivo inexistente.\n");
+        return 0;
+    }
+
+    int *bin = (int *) calloc (32, sizeof(int));
+	for (int i = 0; fscanf(fp, "%u", &inst) != EOF; i += 4) {
+        dec_to_bin(inst, bin);
+        RAM_write(RAM, i, bin);    
+	}		
+    				
+	free(bin);
+	fclose(fp);
+    return 1;
+}
 
 int main (int argc, char *argv[]) {
 
 	// declaracao de variaveis e ponteiros
 
-	int i, sim_error = 0;
 	int **RAM = NULL;
-	FILE *fp = NULL;
-	unsigned int inst;
+    IR *ir = NULL;
+    REGS *regs = NULL;
 
 	if (argc < 2) {
 		printf("uso: ./cpu_multi_code <arquivo>.bin\n");
 		return 1;
 	}
 
-	RAM = (int **) calloc (R, sizeof(int *));
-	for (i = 0; i < R; ++i) RAM[i] = (int *) calloc (8, sizeof(int));
+    // iniciando modulos da cpu
+    init_cpu(&RAM, &ir, &regs);
+	// leitura do arquivo
+    if(readfile(argv[1], RAM) != 0) {
+//        for(int i = 0; i < R; i ++) {
+//            printf("[%d] - %d", i, bin_to_dec(RAM[i], 0, 7)); // impressao em decimal
+////            for(int j = 0; j < 8; j++) { // impressao em binario
+////                printf("%d", RAM[i][j]); //
+////            }                             //
+//            printf("\n");
+//        }
+//        printf("\n");
+    //    run_simulation(RAM, ir, regs);
+	}
 
-	// leitura do arquivo binario
-
-	fp = fopen(argv[1], "r");
-
-	/*
-		sim_error lida com erros surgidos durante a simulacao, causados pela decodificacao de uma
-		instrucao invalida. O seu valor eh definido a depender do erro ocorrido. Se sim_error = 0,
-		nenhum erro apareceu durante a execucao.
-	*/
-
-	while ((fscanf(fp, "%u", &inst) != EOF) || (sim_error > 0)) {
-
-	}						
-	
-	fclose(fp);
-	for (i = 0; i < R; ++i) free(RAM[i]);
-	free(RAM);
+    free_cpu(&RAM, &ir, &regs);
 
 	return 0;
 }
